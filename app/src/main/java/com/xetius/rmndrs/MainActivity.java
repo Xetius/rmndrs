93aa59c96559db63dff65de9e3e5985557f81900
Chris Hudson <chris@xetius.com>
2018-04-20 19:31:12 +0100

    Fixed missing default for switch

--- app/src/main/java/com/xetius/rmndrs/MainActivity.java
+++ app/src/main/java/com/xetius/rmndrs/MainActivity.java
@@ -27,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
+                default:
+                    return false;
             }
-            return false;
         });
     }
 }
