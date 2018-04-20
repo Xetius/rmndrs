6d4c7c08dc4463a41c91dfc56fbee2173bd6139f
Chris Hudson <chris@xetius.com>
2018-04-20 19:26:54 +0100

    Updated anonymous class to lambda

--- app/src/main/java/com/xetius/rmndrs/MainActivity.java
+++ app/src/main/java/com/xetius/rmndrs/MainActivity.java
@@ -1,30 +1,13 @@
 package com.xetius.rmndrs;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = (item) -> {
-        switch (item.getItemId()) {
-            case R.id.navigation_home:
-                mTextMessage.setText(R.string.title_home);
-                return true;
-            case R.id.navigation_dashboard:
-                mTextMessage.setText(R.string.title_dashboard);
-                return true;
-            case R.id.navigation_notifications:
-                mTextMessage.setText(R.string.title_notifications);
-                return true;
-        }
-        return false;
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        });
     }
-
 }
